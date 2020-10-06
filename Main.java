800829bcbcdb66096e89604b056a88b9bfd9a06d
younj1601 <younj1601@wrdsb.ca>
2020-10-06 15:51:41 +0000

    Finished the fifth task for 2.03

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Using a number between one and ten it will output the right amount of stars
+ * @author Jackson
  */
 public class Main {
 
@@ -9,7 +10,17 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //scanner for input
+    Scanner input = new Scanner(System.in);
+
+    //get the number from the user
+    System.out.println("Please enter a number between 1 and 10");
+    int num = input.nextInt();
+
+    //For loop to calculate star output
+    for(int count = 1; count <= num; count++){
+      System.out.print("*");
+    }
     
   }
 }
